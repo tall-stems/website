34ab0c4811a343c72c9c57f98310f4723c1c4aff
tall-stems <michael.edwards.dev@gmail.com>
2022-09-02 17:41:52 -0600

    feat(nextjs-tutorial): adding pre rendering options
    adding tailwind css and performing basic setup
    adding postcss
    adding graymatter
    adding autoprefixer
    adding classnames
    adding sass - need to update files to scss still

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 
+/** @type {import('next').NextConfig} */
 module.exports = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
 	},
+	reactStrictMode: true,
 };
